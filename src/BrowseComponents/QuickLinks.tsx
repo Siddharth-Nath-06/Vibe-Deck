5f38ce65a6db3f2a46ecf602bc305dccbd67660f
Siddharth Nath <siddharthn416@gmail.com>
2025-08-03 17:49:16 +0530

    Change layout of searchbar and quicklinks

--- src/BrowseComponents/QuickLinks.tsx
+++ src/BrowseComponents/QuickLinks.tsx
@@ -6,20 +6,23 @@ function QuickLinks() {
         {t:"Drive", l: "https://drive.google.com"},
         {t: "X", l: "https://x.com"},
         {t: "Twitch", l:"https://twitch.tv"},
-        {t: "YouTube", l: "https://youtube.com"}];
+        {t: "YouTube", l: "https://youtube.com"},
+        {t: "Instagram", l: "https://instagram.com"},
+        {t: "Wikipedia", l: "https://wikipedia.com"},
+        {t: "Some Utimate Long Name Site", l: "https://example.com"}];
 
   return (
-    <div className="grid grid-flow-row grid-cols-4 gap-4 place-items-center w-1/2 pt-6 place-self-center">
+    <div className="grid grid-flow-row grid-cols-6 gap-4 place-items-center w-9/20 pt-6 place-self-center">
         {links.map(e => {
-            return <a href={e.l} className="">
-                <img className="size-12 place-self-center mb-2 rounded-md border border-solid"></img>
-                <h4>{e.t}</h4>
+            return <a href={e.l} className="w-full">
+                <img className="size-15 place-self-center mb-2 rounded-md border border-solid"></img>
+                <h4 className="font-medium truncate">{e.t}</h4>
             </a>
         })}
         {
-            (links.length < 8) && <div>
-                <img className="size-12 place-self-center mb-2 rounded-md border border-solid"></img>
-                <h4>Add</h4>
+            (links.length < 12) && <div>
+                <img className="size-15 place-self-center mb-2 rounded-md border border-solid"></img>
+                <h4 className="font-medium">Add</h4>
                 </div>
         }
     </div>
--- src/BrowseComponents/SearchBar.tsx
+++ src/BrowseComponents/SearchBar.tsx
@@ -4,7 +4,7 @@ function SearchBar() {
 
   return (
     <input type="text" placeholder="Search..."
-    className="h-15 w-1/2 border border-solid rounded-sm mt-4 p-4 font-serif text-lg"
+    className="h-15 w-9/20 border border-solid rounded-sm mt-4 p-4 font-serif text-lg"
     >
     </input>
   )
